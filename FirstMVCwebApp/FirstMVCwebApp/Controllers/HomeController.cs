50f9d71fecb21d96e26511b160b1f584d5d08828
surron <31905175+surron@users.noreply.github.com>
2018-02-12 15:12:25 -0500

    Lab42
    First MVC lab

---
+++ FirstMVCwebApp/FirstMVCwebApp/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMVCwebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult CalcApp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CalcApp(string valA, string valb, string op)
+        {
+            var total = 0;
+            var results = "";
+            
+                var numOne = Request["value1"];
+                var numTwo = Request["value2"];
+                var mod = Request.Form["modifier"];
+
+                int numberOne = Convert.ToInt32(numOne);
+                int numberTwo = Convert.ToInt32(numTwo);
+
+                if ((numberOne | numberTwo) == null)
+                {
+                    results = "ERROR there are no numbers!";
+                }
+                else if (mod == "add")
+                {
+                    total = numberOne + numberTwo;
+                }
+                else if (mod == "subtract")
+                {
+                    total = numberOne - numberTwo;
+                }
+                else if (mod == "multiply")
+                {
+                    total = numberOne * numberTwo;
+                }
+                else if (mod == "divide")
+                {
+                    total = numberOne / numberTwo;
+                }
+                results = Convert.ToString(total);
+            
+            return View("CalcApp", (object)results);
+        }
+    }
+}
---
+++ FirstMVCwebApp/FirstMVCwebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FirstMVCwebApp.MvcApplication" Language="C#" %>
