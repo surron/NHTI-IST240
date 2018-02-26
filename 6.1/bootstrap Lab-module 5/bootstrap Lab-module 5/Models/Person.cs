5e8b0b2d51b64a09d2cdbd4f9d3f7cedaba47dac
surron <31905175+surron@users.noreply.github.com>
2018-02-26 15:31:23 -0500

    6.1

---
+++ 6.1/bootstrap Lab-module 5/bootstrap Lab-module 5/Models/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace bootstrap_Lab_module_5.Models
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ 6.1/bootstrap Lab-module 5/bootstrap Lab-module 5/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="bootstrap_Lab_module_5.MvcApplication" Language="C#" %>
